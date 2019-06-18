70e679e85649aaa845754d353ed3d11321f22e96
Horin <wyhorin@163.com>
2019-06-18 12:27:37 +0800

    '显示手牌end'

--- components/ui.js
+++ components/ui.js
@@ -15,11 +15,16 @@ Vue.component('top-bar', {
 })
 
 Vue.component('card', {
-    template: `<div class="card" :class="'type-' + def.type">
+    template: `<div class="card" :class="'type-' + def.type" @click='play'>
     <div class="title">{{ def.title }}</div>
     <img class="separator" src="svg/card-separator.svg" />
     <div class="description"><div v-html="def.description"></div></div>
     <div class="note" v-if="def.note"><div v-html="def.note"></div></div>
     </div>`,
     props: ['def'],
+    methods: {
+        play () {
+            this.$emit('play', 'orange', '12')
+        }
+    },
 })
--- main.js
+++ main.js
@@ -6,7 +6,7 @@ new Vue({
 
     template: `<div id="#app">
     <top-bar :turn="turn" :current-player-index="currentPlayerIndex" :players="players" />
-    <card :def="testCard" />
+    <card :def="testCard" @play="handlePlay" />
     </div>`,
 
     // 比较实例数据对象和全局state对象
@@ -19,8 +19,13 @@ new Vue({
             return cards.archers
         },
     },
-})
 
+    methods: {
+        handlePlay(color, number) {
+            console.log('You played a card!', 'color=', color, 'number=', number)
+        },
+    },
+})
 
 // 窗口大小变化的处理
 window.addEventListener('resize', () => {
